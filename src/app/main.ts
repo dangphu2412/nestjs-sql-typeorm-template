5838a4f113bf115edf22ace62d9dacd2b2007f06
dnphu <phu.ngoc.dang@mgm-tp.com>
2021-12-07 11:34:36 +0700

    Core: add helmet

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,13 +4,15 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from 'src/external/config/config.service';
 import { AppModule } from './app.module';
 import { configSwagger } from './configs/swagger.config';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = ConfigService.getInt('PORT') ?? 3000;
 
-  app.useGlobalPipes(new ValidationPipe());
   app.enableCors(getCorsConfig());
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
 
   configSwagger(app, 'docs');
 
